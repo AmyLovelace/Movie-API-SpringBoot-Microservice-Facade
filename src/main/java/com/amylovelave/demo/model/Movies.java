10a0a2696d1aa1998296e06513321f1ae3fc194f
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2023-11-15 23:43:18 -0300

    added annotations

--- src/main/java/com/amylovelave/demo/model/Movies.java
+++ src/main/java/com/amylovelave/demo/model/Movies.java
@@ -1,17 +1,33 @@
 package com.amylovelave.demo.model;
 
+import lombok.*;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "Movies")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Movies {
 
+    @Id
+    @Column
     int id;
+
+    @Column
     String name;
+
+    @Column
     String Director;
+
+    @Column
     int rating;
 
 
